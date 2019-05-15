5e509c03811f1a3eb70577408db6bdd4f895c379
luksave <lucasfelipems@gmail.com>
2019-05-15 16:05:16 -0300

    Upd: Correções no exemplo

--- RDF_Jena/Statements/tutorial_01_statements.java
+++ RDF_Jena/Statements/tutorial_01_statements.java
@@ -3,51 +3,54 @@ package jena.examples.rdf ;
 import org.apache.jena.rdf.model.*;
 import org.apache.jena.vocabulary.*;
 
-/** Tutorial 1 creating a simple model
+/** Tutorial 1 Criando um modelo simples e listando suas sentenças
  */
 
 public class Tutorial01 extends Object {
-    // some definitions
+    // Algumas definições 
     static String personURI    = "http://somewhere/LucasFelipe";
     static String givenName    = "Lucas Felipe";
     static String familyName   = "Moreira Silva";
     static String fullName     = givenName + " " + familyName;
     
       public static void main (String args[]) {
-        // create an empty model
+        // Cria um modelo vazio
         Model model = ModelFactory.createDefaultModel();
 
-       // create the resource and add properties (estilo cascata)
-       Resource lucasFelipe = model.createResource(personURI)
+       	// Cria o recurso e adiciona algumas propriedades (estilo cascata)
+       	Resource lucasFelipe = model.createResource(personURI)
 								 .addProperty(VCARD.FN, fullName)
 								 .addProperty(VCARD.N,
 											  model.createResource()
 												   .addProperty(VCARD.Given, givenName)
 												   .addProperty(VCARD.Family, familyName));
       
-      }
+      
 
-		// list the statements in the Model
-		StmtIterator iter = model.listStatements();
+				// Lista as sentenças do modelo através da interface do iterador
+				StmtIterator iter = model.listStatements();
 
-		// print out the predicate, subject and object of each statement
-		while (iter.hasNext()) {
-			Statement stmt      = iter.nextStatement();  // get next statement
-			Resource  subject   = stmt.getSubject();     // get the subject
-			Property  predicate = stmt.getPredicate();   // get the predicate
-			RDFNode   object    = stmt.getObject();      // get the object
+				// Dá como saída o predicado, sujeito e objeto de cada sentença
+				while (iter.hasNext()) {
+					Statement stmt      = iter.nextStatement();  // Obtém a próxima sentença
+					Resource  subject   = stmt.getSubject();     // Obtém o sujeito da sentença
+					Property  predicate = stmt.getPredicate();   // Obtém o predicado da sentença
+					RDFNode   object    = stmt.getObject();      // Obtém o objeto da sentença
 
-			System.out.print(subject.toString());
-			System.out.print(" " + predicate.toString() + " ");
-			
-			if (object instanceof Resource) 
-				System.out.print(object.toString());
+					System.out.print(subject.toString());
+					System.out.print(" " + predicate.toString() + " ");
+					
+					if (object instanceof Resource) 
+						System.out.print(object.toString());
 
-			// object is a literal
-			else 
-				System.out.print(" \"" + object.toString() + "\"");
+					// Se o objeto é na verdade um literal
+					else 
+						System.out.print(" \"" + object.toString() + "\"");
 
-			System.out.println(" .");
-		} 
+					System.out.println(" .");
+
+				} 
+
+		}
 
 }
