a5fa43e119dff4c795a70e57471360ec9017dc4e
Lucas Felipe <30655133+luksave@users.noreply.github.com>
2019-05-16 09:48:26 -0300

    Upd: Mellhora de identação 3

--- RDF_Jena/Model and Properties/tutorial_02_model_properties.java
+++ RDF_Jena/Model and Properties/tutorial_02_model_properties.java
@@ -3,29 +3,29 @@ package jena.examples.rdf ;
 import org.apache.jena.rdf.model.*;
 import org.apache.jena.vocabulary.*;
 
-/** Tutorial 2 criando um modelo e adicionando propriedades a um recurso
+/** Tutorial 2 - Criando um modelo e adicionando propriedades a um recurso
  */
 
 public class Tutorial01 extends Object {
     
 	//Algumas definições
-    static String personURI    = "http://somewhere/LucasFelipe";
-    static String givenName    = "Lucas Felipe";
-    static String familyName   = "Moreira Silva";
-    static String fullName     = givenName + " " + familyName;
-    
-      public static void main (String args[]) {
-        
-		//Modelo vazio criado
-        Model model = ModelFactory.createDefaultModel();
+	static String personURI    = "http://somewhere/LucasFelipe";
+	static String givenName    = "Lucas Felipe";
+	static String familyName   = "Moreira Silva";
+	static String fullName     = givenName + " " + familyName;
 
-       // Criando o recurso lucasfelipe e adicionando propriedades a ele (estilo cascata)
-       Resource lucasFelipe = model.createResource(personURI)
-								 .addProperty(VCARD.FN, fullName)
-								 .addProperty(VCARD.N,
-											  model.createResource()
-												   .addProperty(VCARD.Given, givenName)
-												   .addProperty(VCARD.Family, familyName));
-      
-      }
+	public static void main (String args[]) {
+
+	//Modelo vazio criado
+	Model model = ModelFactory.createDefaultModel();
+
+	// Criando o recurso lucasfelipe e adicionando propriedades a ele (estilo cascata)
+	Resource lucasFelipe = model.createResource(personURI)
+			 .addProperty(VCARD.FN, fullName)
+			 .addProperty(VCARD.N, model.createResource()
+						   .addProperty(VCARD.Given, givenName)
+						   .addProperty(VCARD.Family, familyName));
+
+	}
+
 }
