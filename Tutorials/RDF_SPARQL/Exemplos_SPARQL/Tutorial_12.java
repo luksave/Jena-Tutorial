ecfeb78321cf05157f2a791e06abdadd00ff1dcc
Lucas Felipe <30655133+luksave@users.noreply.github.com>
2019-05-23 09:52:30 -0300

    Upd: Melhoria de identação 8.12

--- Tutorials/RDF_SPARQL/Exemplos_SPARQL/Tutorial_12.java
+++ Tutorials/RDF_SPARQL/Exemplos_SPARQL/Tutorial_12.java
@@ -33,14 +33,14 @@ public class Tutorial12 {
 	public static void main (String args[]) {
 
 		//Retorna os nomes completos pela propriedade VCARD.FN de todos os recursos e também 
-  	//as idades de maiores de 24 anos, caso possua a propriedade INFO.AGE
-    String query = 	"PREFIX info:    <http://somewhere/peopleInfo#> "+
-				    				"PREFIX vcard:   <http://www.w3.org/2001/vcard-rdf/3.0#> "+
-										"SELECT ?name ?age " +
-										"WHERE " + 
-										"{ ?person vcard:FN  ?name ." +
-												"  OPTIONAL { ?person info:age ?age . FILTER ( ?age > 24 ) }" +
-										"}";      
+		//as idades de maiores de 24 anos, caso possua a propriedade INFO.AGE
+		String query = 	"PREFIX info:    <http://somewhere/peopleInfo#> "+
+				"PREFIX vcard:   <http://www.w3.org/2001/vcard-rdf/3.0#> "+
+				"SELECT ?name ?age " +
+				"WHERE " + 
+				"{ ?person vcard:FN  ?name ." +
+					"  OPTIONAL { ?person info:age ?age . FILTER ( ?age > 24 ) }" +
+				"}";      
 
 
 		/*
@@ -66,7 +66,7 @@ public class Tutorial12 {
 		else if(file == 2)	inputFileName  = "vc-db-2.rdf";
 		else if(file == 3)	inputFileName  = "vc-db-3.rdf";
 		else if(file == 4)	inputFileName  = "vc-db-4.rdf";
-		else								throw new IllegalArgumentException("Arquivo: " + inputFileName + " não encontrado!");
+		else			throw new IllegalArgumentException("Arquivo: " + inputFileName + " não encontrado!");
 
 
 		// Criação de um MODELO RDF vazio
